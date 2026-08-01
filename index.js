e00e9263e4244edaef8920520635bd6cdbdffd88
Shreshtha Sartha <shreshthasartha@Shreshthas-MacBook-Air.local>
2026-08-01 15:19:15 +0530

    Add Post /projects route

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ app.get('/', (req, res) => {
   res.send('Task Manager API is running');
 });
 
+let projects = 
+[
+    {id: 1, name: "Website Redesign"},
+    {id: 2, name: "Marketing Campaign"}
+];
+
+app.get('/projects', (req,res) =>{
+    res.json(projects);
+});
+
+
+app.use(express.json());
+
+app.post('/projects', (req,res) => {
+    const newProject = {
+        id: projects.lenght + 1,
+        name: req.body.name
+    };
+    projects.push(newProject);
+    res.status(201).json(newProject);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
